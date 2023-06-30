fcca767370f8676f3fe0647ff6a6ac78b497e8d6
RaenonX <raenonx0710@gmail.com>
2023-06-30 14:00:46 -0500

    Added calculated px exporting API EP

--- MSM.TS/Controllers/PxController.cs
+++ MSM.TS/Controllers/PxController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using MSM.Calc.Computer;
 using MSM.Common.Controllers;
 using MSM.Common.Utils;
 using MSM.TS.Payloads;
@@ -24,5 +25,34 @@ public class PxController : ControllerBase {
 
         await PxTickController.RecordPx(payload.Item, payload.Px);
         return Ok();
+    }
+
+    [HttpGet]
+    public async Task<FileResult> GetCalculatedBars(
+        [FromQuery] string item,
+        [FromQuery] DateTime start,
+        [FromQuery] DateTime? end,
+        [FromQuery] int intervalMin
+    ) {
+        using var stream = new MemoryStream();
+        await using TextWriter writer = new StreamWriter(stream);
+
+        await writer.WriteLineAsync("Time,Open,High,Low,Close,UpTick,DownTick");
+        var bars = PxDataAggregator.GetBarsAsync(item, start, end ?? DateTime.UtcNow, intervalMin);
+        
+        foreach (var row in await bars) {
+            await writer.WriteLineAsync(
+                $"{row.Timestamp},{row.Open},{row.High},{row.Low},{row.Close},{row.UpTick},{row.DownTick}"
+            );
+        }
+
+        await writer.FlushAsync();
+        stream.Position = 0;
+
+        return File(
+            stream.ToArray(),
+            "text/csv",
+            $"{item}-{start:yyyyMMdd}-{end:yyyyMMdd}@{intervalMin}.csv"
+        );
     }
 }
