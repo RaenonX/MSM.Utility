a2430880a9984a12c059194987e55cc5ba8c5d6a
RaenonX <raenonx0710@gmail.com>
2023-07-01 09:11:21 -0500

    Added API access log

--- MSM.TS/Controllers/ItemController.cs
+++ MSM.TS/Controllers/ItemController.cs
@@ -7,8 +7,16 @@ namespace MSM.TS.Controllers;
 [ApiController]
 [Route("/api/[controller]")]
 public class ItemController : ControllerBase {
+    private readonly ILogger<ItemController> _logger;
+
+    public ItemController(ILogger<ItemController> logger) {
+        _logger = logger;
+    }
+
     [HttpGet]
     public async Task<JsonResult> GetAvailableItems() {
+        _logger.LogInformation("Getting available items");
+
         return new JsonResult(new AvailableItemsResponse {
             Items = (await PxTickController.GetAvailableItemsAsync()).Order()
         });
--- MSM.TS/Controllers/PxController.cs
+++ MSM.TS/Controllers/PxController.cs
@@ -11,6 +11,12 @@ namespace MSM.TS.Controllers;
 [ApiController]
 [Route("/api/[controller]")]
 public class PxController : ControllerBase {
+    private readonly ILogger<PxController> _logger;
+
+    public PxController(ILogger<PxController> logger) {
+        _logger = logger;
+    }
+
     [HttpPost]
     public async Task<ActionResult> RecordPx([FromForm] PxRecordPayload payload) {
         if (payload.Token != ConfigHelper.GetApiToken()) {
@@ -34,6 +40,14 @@ public class PxController : ControllerBase {
         [FromQuery] DateTime? end,
         [FromQuery] int intervalMin
     ) {
+        _logger.LogInformation(
+            "Getting calculated bars of {Item} in {IntervalMin} mins interval ({Start} ~ {End})",
+            item,
+            intervalMin,
+            start,
+            end
+        );
+
         return new JsonResult(new PxBarResponse {
             Item = item,
             Bars = await PxDataAggregator.GetBarsAsync(item, start, end, intervalMin)
