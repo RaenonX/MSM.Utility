347c4d5c4b260af32b0b9473defb8dfff5c5a973
RaenonX <raenonx0710@gmail.com>
2023-07-03 03:44:17 -0500

    Added price update recovered message

--- MSM.Bot/Workers/PxUpdateCheckWorker.cs
+++ MSM.Bot/Workers/PxUpdateCheckWorker.cs
@@ -9,6 +9,8 @@ public class PxUpdateCheckWorker : BackgroundService {
 
     private readonly ILogger<PxUpdateCheckWorker> _logger;
 
+    private bool _failed;
+
     public PxUpdateCheckWorker(DiscordSocketClient client, ILogger<PxUpdateCheckWorker> logger) {
         _client = client;
         _logger = logger;
@@ -21,9 +23,12 @@ public class PxUpdateCheckWorker : BackgroundService {
             var lastValidTickUpdate = await PxMetaController.GetLastValidTickUpdate();
 
             if (lastValidTickUpdate is null) {
+                // No valid tick
                 _logger.LogWarning("Last valid tick check failed (No valid tick)");
                 await channel.SendMessageAsync("No last valid tick update found!");
+                _failed = true;
             } else if (DateTime.UtcNow - lastValidTickUpdate > TimeSpan.FromSeconds(45)) {
+                // No valid tick within certain time
                 var secsAgo = (DateTime.UtcNow - lastValidTickUpdate.Value).TotalSeconds;
 
                 _logger.LogWarning(
@@ -34,12 +39,23 @@ public class PxUpdateCheckWorker : BackgroundService {
                     $"No price update since **{secsAgo:0} secs ago**!\n" +
                     $"> Last valid tick updated at {lastValidTickUpdate} (UTC)"
                 );
+                _failed = true;
             } else {
+                // Found valid tick
+                if (_failed) {
+                    await channel.SendMessageAsync(
+                        $"Prices start ticking again!\n" +
+                        $"> Received price update at **{lastValidTickUpdate}** (UTC)"
+                    );
+                }
+
                 _logger.LogInformation(
                     "Last valid tick check succeed (Last tick at {LastValidTickUpdate})",
                     lastValidTickUpdate
                 );
+                _failed = false;
             }
+
             await Task.Delay(30000, cancellationToken);
         }
     }
